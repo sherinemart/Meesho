846da8f90997819ff9df9a5e2ff0631f52d0d0b7
sherinemart <91425378+sherinemart@users.noreply.github.com>
2023-07-11 14:13:46 +0530

    initial commit

---
+++ Arith.java
@@ -0,0 +1,19 @@
+class Arith
+{
+ public static void main(String args[])
+ {
+ int a=10;
+ int b=0;
+ int c;
+try
+{
+ c=a/b;
+ System.out.println("The Result is: "+c);
+}catch(ArithmeticException e)
+{
+ 
+ System.out.println("you cannot divide by zero");
+}
+ System.out.println("Rest of the code");
+}
+}
